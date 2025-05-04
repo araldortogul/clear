49a02a5c14b8c73ddd3458d8cf8b85a07d9d6850
araldortogul <araldortogul@gmail.com>
2025-05-04 23:15:32 +0200

    feat: add middleware for multilingual support

---
+++ src/middleware.ts
@@ -0,0 +1,44 @@
+import { match } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
+import { NextRequest, NextResponse } from 'next/server';
+
+const locales = ['en', 'tr'];
+
+// Get the preferred locale, similar to the above or using a library
+// TODO:
+//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getLocale = (request: NextRequest) => {
+  const headers: Negotiator.Headers = { 'accept-language': 'en-US,en;q=0.5' };
+  const languages = new Negotiator({ headers }).languages();
+  const defaultLocale = 'en';
+
+  const matchedLocale = match(languages, locales, defaultLocale); // -> 'en'
+  return matchedLocale;
+};
+
+export const middleware = (request: NextRequest) => {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  );
+
+  if (pathnameHasLocale) return;
+
+  // Redirect if there is no locale
+  const locale = getLocale(request);
+
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  return NextResponse.redirect(request.nextUrl);
+};
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    '/((?!_next).*)',
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
